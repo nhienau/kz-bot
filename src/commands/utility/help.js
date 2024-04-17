93083b5ff6306484c9dfbe81ad05c94b3fc73637
Nhien <nhienau@gmail.com>
2024-04-17 18:35:06 +0700

    Update /help message

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -7,19 +7,19 @@ module.exports = {
     .setName("help")
     .setDescription("Danh sách các lệnh"),
   async execute(interaction) {
-    const message = `\`/1v1d3llz\` - Ảnh về Hồng Huy Hoàng
-    \`/bach\` - Ảnh của chủ server
-    \`/bubble\` - Bong bóng
-    \`/kz\` - Ảnh của tất cả anh em Kz
-    \`/meme\` - Những câu chat, comment, hình vớ vẩn của anh em Kz
-    \`/pick\` - Chọn 1 lựa chọn ngẫu nhiên
-    \`/random\` - Chọn 1 số ngẫu nhiên trong khoảng chỉ định
-    \`/shitpost\` - Shitpost, văn mẫu
-    \`/video\` - Video
-    \`/when\` - Dùng khi có ai hỏi "khi nào"
-    Anh em sử dụng bot lưu ý không spam lệnh liên tục (cách khoảng 3-5s).
-    Nếu có ý tưởng lệnh mới hoặc muốn đóng góp hình ảnh, shitpost thì ping <@${process.env.OWNER_ID}>`;
-
+    const message =
+      "`/1v1d3llz` - Ảnh về Hồng Huy Hoàng\n" +
+      "`/bach` - Ảnh của chủ server\n" +
+      "`/bubble` - Bong bóng\n" +
+      "`/kz` - Ảnh anh em Kz\n" +
+      "`/meme` - Chat, comment, hình linh tinh\n" +
+      "`/pick` - Chọn 1 lựa chọn ngẫu nhiên\n" +
+      "`/random` - Chọn 1 số ngẫu nhiên trong khoảng chỉ định\n" +
+      "`/shitpost` - Shitpost, văn mẫu\n" +
+      "`/video` - Video\n" +
+      '`/when` - Dùng khi có ai hỏi "khi nào"\n' +
+      "Anh em sử dụng bot lưu ý không spam lệnh liên tục (cách khoảng 3-5s).\n" +
+      `Báo lỗi, góp ý tính năng mới hoặc đóng góp hình ảnh, shitpost: ping <@${process.env.OWNER_ID}>.`;
     const embed = new EmbedBuilder().setDescription(message);
     await interaction.reply({ embeds: [embed] });
   },
