3533ab66eb4558f39c0b15f911ecfde2fb57648e
Nhien <nhienau@gmail.com>
2024-04-19 15:28:38 +0700

    Add message as optional arg to bubble command

--- src/commands/utility/bubble.js
+++ src/commands/utility/bubble.js
@@ -5,9 +5,13 @@ module.exports = {
   cooldown: 5,
   data: new SlashCommandBuilder()
     .setName("bubble")
-    .setDescription("Speech bubble"),
+    .setDescription("Speech bubble")
+    .addStringOption((option) =>
+      option.setName("message").setDescription("Message (optional)")
+    ),
   async execute(interaction) {
     const imgUrls = getContentByTopic("bubble");
+    const optionalMessage = interaction.options.getString("message");
 
     if (imgUrls.length === 0) {
       await interaction.reply("Chưa có dữ liệu.");
@@ -15,15 +19,21 @@ module.exports = {
     }
 
     const randomIndex = Math.floor(Math.random() * (imgUrls.length - 1));
-    const messages = await interaction.channel.messages.fetch({ limit: 15 });
-    const message = messages.find(
-      (message) => !message.author.bot && message.content !== ""
-    );
-    if (!message) {
-      await interaction.reply(imgUrls[randomIndex]);
-      return;
+
+    let messageContent;
+    if (optionalMessage) {
+      messageContent = optionalMessage;
+    } else {
+      const messages = await interaction.channel.messages.fetch({ limit: 15 });
+      const message = messages.find(
+        (message) => !message.author.bot && message.content !== ""
+      );
+      if (!message) {
+        await interaction.reply(imgUrls[randomIndex]);
+        return;
+      }
+      messageContent = message.content;
     }
-    const messageContent = message.content;
     const embed = new EmbedBuilder()
       .setDescription(messageContent)
       .setImage(imgUrls[randomIndex]);
