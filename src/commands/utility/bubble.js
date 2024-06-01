34ee1f602b253e4c51b9b493db1d5b60a7080ae7
Nhien <nhienau@gmail.com>
2024-06-02 01:05:28 +0700

    Reduce cooldown amount for some commands

--- src/commands/utility/bubble.js
+++ src/commands/utility/bubble.js
@@ -4,7 +4,7 @@ const getContentByTopic = require("../../helpers/getContentByTopic.js");
 const random = require("../../helpers/random.js");
 
 module.exports = {
-  cooldown: 15,
+  cooldown: 10,
   data: new SlashCommandBuilder()
     .setName("bubble")
     .setDescription("Speech bubble")
