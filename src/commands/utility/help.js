711973a49d88f3429790065f2b101d63d808ff83
Nhien <nhienau@gmail.com>
2024-07-02 13:25:15 +0700

    Refactor /help command list

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+const listCommands = require("../../helpers/listCommands.js");
 
 module.exports = {
   cooldown: 5,
@@ -7,18 +8,9 @@ module.exports = {
     .setName("help")
     .setDescription("Danh sách các lệnh"),
   async execute(interaction) {
-    const message =
-      "`/1v1d3llz` - Ảnh về Hồng Huy Hoàng\n" +
-      "`/bach` - Ảnh của chủ server\n" +
-      "`/bubble` - Bong bóng\n" +
-      "`/kz` - Ảnh anh em Kz\n" +
-      "`/meme` - Chat, comment, hình linh tinh\n" +
-      "`/pick` - Chọn 1 lựa chọn ngẫu nhiên\n" +
-      "`/raisehand` - Dùng khi có ai chuẩn bị nói gì đó\n" +
-      "`/random` - Chọn 1 số ngẫu nhiên trong khoảng chỉ định\n" +
-      "`/shitpost` - Shitpost, văn mẫu\n" +
-      "`/video` - Video\n" +
-      '`/when` - Dùng khi có ai hỏi "khi nào"\n';
+    const message = listCommands
+      .map((command) => `\`/${command.name}\` - ${command.description}\n`)
+      .join("");
     const embed = new EmbedBuilder().setDescription(message);
     await interaction.reply({ embeds: [embed] });
   },
